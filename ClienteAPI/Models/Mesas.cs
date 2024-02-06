998b9ba23f9623c039869fec3303ac798d7c81ab
Miguel Aceituno Rojo <Miguel Aceituno Rojo@DESKTOP-64UCGFH>
2024-02-05 21:58:55 -0500

    Creando las clases mesas y correción de codigo extra

---
+++ ClienteAPI/Models/Mesas.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace ClienteAPI.Models
+{
+    public class Mesas
+    {
+        public int NumeroPatas { get; set; }
+        public Color Color { get; set; }
+
+    }
+}
